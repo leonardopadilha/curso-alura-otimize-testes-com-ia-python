1c625fbfbd6bff40b28ee2c851cda08bd67ea443
Leonardo Padilha <leonardopadilha28@gmail.com>
2026-07-15 20:13:33 -0300

    Implementando a base dados vetorial com Faiss

---
+++ faiss_projeto.py
@@ -0,0 +1,44 @@
+from openai import OpenAI
+from tools import *
+from dotenv import load_dotenv
+import faiss
+import json
+import os
+from gerador_embedding import *
+import numpy as np
+
+load_dotenv()
+
+cliente = OpenAI()
+
+FAISS_INDEX_SIZE = 1536 # para text-embedding-3-small, já para text-embedding-3-large é 3072
+DIRETORIO_DOCUMENTOS = "AcordeLab"
+CAMINHO_DADOS_FAISS = "projeto.index"
+CAMINHO_METADADOS = "metadados_projeto.json"
+
+def ler_criar_indice_faiss(caminho_arquivo, tamanho_dimensoes):
+    if os.path.exists(caminho_arquivo):
+        print(f"\nÍndice já existe, carregando...")
+        return faiss.read_index(caminho_arquivo)
+    else:
+        print("\nCriando novo índice...")
+        return faiss.IndexFlatL2(tamanho_dimensoes)
+
+def criar_dados_faiss(caminho_dados_faiss, descricoes, metadados, caminho_metadados):
+    dados_faiss = ler_criar_indice_faiss(caminho_dados_faiss, FAISS_INDEX_SIZE)
+
+    if not os.path.exists(caminho_dados_faiss):
+        dados_faiss = adicionar_indice_embedding(dados_faiss, descricoes, metadados, caminho_metadados)
+        faiss.write_index(dados_faiss, caminho_dados_faiss)
+
+    return dados_faiss
+
+def adicionar_indice_embedding(dados_faiss, descricoes, metadados, caminho_metadados):
+    embeddings = np.array([gerar_embedding(texto) for texto in descricoes]).astype('float32')
+    faiss.normalize_L2(embeddings)
+    dados_faiss.add(embeddings)
+
+    with open(caminho_metadados, 'w', encoding='utf-8') as arquivo_saida:
+        json.dump(metadados, arquivo_saida, ensure_ascii=False, indent=4)
+
+    return dados_faiss
